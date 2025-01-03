4a00929fc2fce2aec1c4c3316084fde141fa9604
camain-elie <camain.elie@gmail.com>
2025-01-03 19:57:35 +1100

    Add incomplete day 6 part 2

--- Day 6/part2.js
+++ Day 6/part2.js
@@ -2,58 +2,121 @@ const fs = require('fs')
 
 const input = fs.readFileSync('./input', 'utf-8')
 
-const rulesInput = input.match(/[0-9]+\|[0-9]+/g)
-const pagesInput = input.match(/([0-9])+,([0-9][0-9],)*([0-9])+/g)
+// INIT
 
-const rules = {}
-rulesInput.forEach(item => {
-    const [pre, post] = item.split('|')
-    rules[pre] = [...(rules[pre] ?? []), post]
+console.time('init')
+
+const lineArray = input.split('\r\n')
+const map = lineArray.map(item => item.split(''))
+const obstacleMap = [...lineArray].map(item => item.split(''))
+const direction = [
+    {x: 0, y: -1},
+    {x: 1, y: 0},
+    {x: 0, y: 1},
+    {x: -1, y: 0},
+]
+const directionChar = ['↑', '→', '↓', '←']
+let directionIndex = 0
+let position = {x: -1, y: -1}, initialPosition
+let directionChanged = false
+
+map.forEach((item, index) => {
+    const x = item.indexOf('^')
+    if (x !== -1) {
+        position.x = x
+        position.y = index
+    }
 })
 
-const pages = pagesInput.map(item => item.split(','))
+initialPosition = {...position}
 
-const wrongUpdates = [], pbNumber = [], fixedUpdates = []
+console.timeEnd('init')
 
-function checkUpdate(update) {
-    let isValid = true, fixedArray
+// FUNCTIONS
 
-    update.forEach((pageNumber, index, update) => {        
-        for(let i = index; i<update.length-1; i++) {
-            let rule = rules[update[i]]
-            if (rule && rule.indexOf(pageNumber) !== -1) {
-              pbNumber.push(pageNumber)
-              isValid = false
-
-              // try to fix the array
-              if (!fixedArray) {
-                fixedArray = [...update]
-                fixedArray.splice(i, 1)
-                fixedArray.splice(index, 0, update[i])
-                // fixedUpdates.push(fixedArray)
-              }
-            }
-        }
-    })
-
-    if (!isValid) {
-      wrongUpdates.push(update)
-      fixedUpdates.push(fixedArray)
-      // pbNumber.push()
-    
+function calculatePath(write) {
+  while (position.x >=0 && position.x < map[0].length && position.y >= 0 && position.y < map.length) {
+    if (checkNextPosObstacle()) {
+      directionIndex = changeDirection(directionIndex)
+      directionChanged = true
+    } else {
+      move(write)
+      directionChanged = false
     }
-    return 0
+  }
 }
 
-let result = 0
+function changeDirection (currentDir) {
+    let newDirection = currentDir + 1
+    if (newDirection > direction.length - 1) newDirection = 0
+    return newDirection
 
-pages.forEach((update) => {
-    result += Number(checkUpdate(update), )
+}
+
+function checkNextPosObstacle () {
+    const {x: dirX, y: dirY} = direction[directionIndex]
+    let pos = {x: position.x + dirX, y: position.y + dirY}
+    return map[pos.y][pos.x] === "#"
+}
+
+function move (write) {
+    let {x: dirX, y: dirY} = direction[directionIndex],
+        {x, y} = position
+    let newPos = { x: x + dirX, y: y + dirY }
+    const char = directionChanged ? '+' : directionChar[directionIndex]
+    if (write) map[y][x] = char
+    else checkAddObstacle(directionIndex, position)
+    position = newPos
+}
+
+function checkAddObstacle ( directionIndex, currentPosition ) {
+  const { x: dirX, y: dirY } = direction[directionIndex],
+    { x, y } = currentPosition
+  const nextX = x + dirX, nextY = y + dirY
+  
+  if (map[nextY] && map[nextY][nextX] !== '#') {
+    let newDir = changeDirection(directionIndex),
+      newPosition = {...currentPosition}
+    const correctChar = directionChar[newDir]
+
+    let directionCondition = false, directionChangeCondition = false
+
+    while (newPosition.x >=0 && newPosition.x < map[0].length && newPosition.y >= 0 && newPosition.y < map.length) {
+      newPosition = { x: direction[newDir].x + newPosition.x, y: direction[newDir].y + newPosition.y }
+      if (map[newPosition.y] && map[newPosition.y][newPosition.x] === correctChar) directionCondition = true
+      if (map[newPosition.y] && map[newPosition.y][newPosition.x] === '#') directionChangeCondition = true 
+    }
+
+
+    if(directionCondition && directionChangeCondition) obstacleMap[nextY][nextX] = 'O'
+  }
+}
+
+function reinitState() {
+  position = initialPosition
+  directionIndex = 0
+  directionChanged = false
+}
+
+// RUN
+
+console.time('path')
+calculatePath(true)
+console.timeEnd('path')
+
+console.time('obstacle')
+reinitState()
+calculatePath(false)
+console.timeEnd('obstacle')
+
+const mapString = map.map(item => item.join('')).join('\n'),
+  obstacleString = obstacleMap.map(item => item.join('')).join('\n')
+
+// answer is above 1602
+
+fs.writeFile('./output.txt', mapString, err => {
+  if (err) console.error(err)
 })
-
-let resultFixedArray = fixedUpdates.reduce((prev, curr, arr) => prev + Number(curr[(curr.length-1)/2]), 0)
-// function correctUpdate(update) {
-
-// }
-
-console.log(result, wrongUpdates[0], pbNumber[0], wrongUpdates.length, fixedUpdates.length, resultFixedArray)
+fs.writeFile('./obstacle.txt', obstacleString, err => {
+  if (err) console.error(err)
+})
